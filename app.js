4354c2fbc3908a32d5601cbdcc02a885c806215f
ty.hranac <ty.hranac@gmail.com>
2022-12-27 12:46:59 -0800

    Toggle background color/color on td click

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const htmlWordList2 = document.querySelector('.wl-second-half');
 // define words
 const words = ['noel', 'merry', 'cheer', 'mistletoe', 'holly', 'santa', 'gifts',
                 'tree', 'wreath', 'candle', 'carol', 'elf', 'candycane'];
+// define alphabet
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                    'y', 'z']
 
 // convert html collection to array
 const tableRows = Array.from(htmlTable.rows);
@@ -17,10 +21,13 @@ let jsTable = Array();
 for (let ir=0; ir<tableRows.length; ir++) {
     let tableRow = Array.from(tableRows[ir].children);
     for (let ic=0; ic<tableRow.length; ic++) {
+        tableRow[ic].addEventListener('click', () => {
+            tableRow[ic].classList.toggle('letter-selected')
+        })
         if (typeof words[ir][ic] !== 'undefined') {
             tableRow[ic].innerText = words[ir][ic];
         } else {
-            tableRow[ic].innerText = 'x';
+            tableRow[ic].innerText = alphabet[Math.floor(Math.random() * alphabet.length)];
         }
     }
     let li = document.createElement('li');
